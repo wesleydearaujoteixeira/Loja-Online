f708530d1daf89911ee2d04d70df8321b50142ff
wesley <wesleyaraujoteixeira98@gmail.com>
2024-07-08 17:31:23 -0300

    creating a second page

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,14 @@
 let menu = document.getElementById('myItens');
 
-menu.style.maxHeight = '150px';
+menu.style.maxHeight = '0px';
 
 
 document.querySelector('.menu').addEventListener('click', () => {
 
-   if(menu.style.maxHeight == '0px') {
-        menu.style.maxHeight = '150px';
-   }else {
+   if(menu.style.maxHeight == '150px') {
         menu.style.maxHeight = '0px';
+   }else {
+        menu.style.maxHeight = '150px';
    }
 
 });
